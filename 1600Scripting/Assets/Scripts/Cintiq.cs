b18a7c8946acbec973073a877ed678313b2984fb
Kino <10818333@my.uvu.edu>
2018-05-23 11:26:46 -0600

    Added tablet class

---
+++ 1600Scripting/Assets/Scripts/Cintiq.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cintiq : Tablet{
+
+	// Use this for initialization
+	void Start () {
+        print("Draws");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1600Scripting/Assets/Scripts/Tablet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tablet : MonoBehaviour {
+
+    public int PressureCount = 2000;
+    [Range(0,5)]public float Sensitivity = 5;
+    
+}
