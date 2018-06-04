47c83552c300f71af39b3aaec607950461e81d5d
Kino <10818333@my.uvu.edu>
2018-06-04 11:20:57 -0600

    Added PlayerTrigger and HealthBehaviour

---
+++ 1600Scripting/Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour
+{
+	public float HealthLevel = 1.0f;
+	public float EvilPower = 0.1f;
+	public HealthBehaviour PlayerHealthBehaviour;
+	// Use this for initialization
+	void Start () {
+	}
+	// Update is called once per frame
+	void OnTriggerEnter ()
+	{
+		HealthLevel -= EvilPower;
+		PlayerHealthBehaviour.HealthLevel = HealthLevel;
+	}
+}
