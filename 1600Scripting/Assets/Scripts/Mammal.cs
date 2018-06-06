f3e0824e5f89f31ca45dc748bb0cb540ac9dac37
Kino <10818333@my.uvu.edu>
2018-06-06 09:54:51 -0600

    Modified Mammal class

--- 1600Scripting/Assets/Scripts/Mammal.cs
+++ 1600Scripting/Assets/Scripts/Mammal.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu]
 public class Mammal : Animal {
 
 
     public int HairCount = 1000;
-    public Color HairColor;
+    public Color HairColor = Color.black;
 
 }
