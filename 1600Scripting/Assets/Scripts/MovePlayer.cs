b27e581e0d3cd8c6ca1b45aea1192dc300c21512
Kino <10818333@my.uvu.edu>
2018-07-05 14:23:44 -0600

    GetKeyDown

--- 1600Scripting/Assets/Scripts/MovePlayer.cs
+++ 1600Scripting/Assets/Scripts/MovePlayer.cs
@@ -8,7 +8,7 @@ public class MovePlayer : MonoBehaviour {
 	private CharacterController controller;
 	private Vector3 newPosition;
 	public float Speed = 10.0f;
-	public float Gravity = -9.81f;
+	public float Gravity = 9.81f;
 	public float JumpSpeed = 10.0f;
 	//private variables begin lowercase 
 	//public variables begin capitalized
@@ -21,12 +21,12 @@ public class MovePlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if (Input.GetKeyDown("Jump"))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			newPosition.y += JumpSpeed;
+			newPosition.y = JumpSpeed;
 		}
-		
-		newPosition.y = Gravity;
+
+		newPosition.y -= Gravity;
 		newPosition.x = Speed*Input.GetAxis("Horizontal");
 		controller.Move(newPosition * Time.deltaTime);
 	}
