6c74f56ed44a2ce86e67ecb71ef6c835f5a18f4d
Kino <10818333@my.uvu.edu>
2018-07-05 17:26:00 -0600

    WeaponType
    Axe, club, sword, spear, scimitar (Or as Anthony says, "SIGH-METER")

---
+++ 1600Scripting/Assets/Scripts/Weapons.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour {
+
+	public enum WeaponType
+	{
+		Axe,
+		Club,
+		Sword,
+		Spear,
+		Scimitar,
+	}
+		
+	public WeaponType CurrentWeapon;
+
+	void Update()
+	{
+		switch (CurrentWeapon)
+		{
+			case WeaponType.Axe:
+			Debug.Log(CurrentWeapon);
+			break;
+
+			case WeaponType.Club:
+			Debug.Log(CurrentWeapon);
+			break;
+
+			case WeaponType.Sword:
+			Debug.Log(CurrentWeapon);
+			break;
+
+			case WeaponType.Spear:
+			Debug.Log(CurrentWeapon);
+			break;
+
+			case WeaponType.Scimitar:
+			Debug.Log(CurrentWeapon);
+			break;
+		}
+	}
+
+}
