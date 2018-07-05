181a41fca0aa175c1e16ccc50d02fbc0804da36c
Kino <10818333@my.uvu.edu>
2018-07-05 16:14:59 -0600

    IfStatements

---
+++ 1600Scripting/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IfStatements : ScriptableObject {
+
+	private void OnEnable()
+    {
+
+        public int a;
+        public int b;
+        public int c;
+        if (2 + 2 == 4) //== is a comparison operator
+                        //= is an assignment operator
+        {
+            Debug.Log("True");
+        }
+    }
+
+}
