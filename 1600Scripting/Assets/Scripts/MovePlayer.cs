0055fc0b81eb59ca425959007a91e9b19fd8c681
Kino <10818333@my.uvu.edu>
2018-07-05 16:28:44 -0600

    isGrounded

--- 1600Scripting/Assets/Scripts/MovePlayer.cs
+++ 1600Scripting/Assets/Scripts/MovePlayer.cs
@@ -22,7 +22,7 @@ public class MovePlayer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
 		{
 			newPosition.y = JumpSpeed;
 		}
