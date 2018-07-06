a4eded4af9ccb54ebc0dc3858b2125d1aeb46534
Kino <10818333@my.uvu.edu>
2018-07-05 18:08:31 -0600

    Collect weapon

--- 1600Scripting/Assets/Scripts/Weapons/WeaponPickUp.cs
+++ 1600Scripting/Assets/Scripts/Weapons/WeaponPickUp.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class WeaponPickUp : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public WeaponObject Weapon;
+	public WeaponsCollection Collection;
+
+	private void OnTriggerEnter(Collider obj)
+	{
+		 Collection.CollectWeapon(Weapon);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
--- 1600Scripting/Assets/Scripts/Weapons/WeaponsCollection.cs
+++ 1600Scripting/Assets/Scripts/Weapons/WeaponsCollection.cs
@@ -10,5 +10,9 @@ using UnityEngine;
 public class WeaponsCollection : ScriptableObject {
 
     public List<WeaponObject> Weapons;
+    public void CollectWeapon(WeaponObject weapon)
+    {
+        Debug.Log(weapon);
+    }
 
 }
