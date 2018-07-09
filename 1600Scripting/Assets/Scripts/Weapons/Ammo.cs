d5059a9f6cd871c838a6d2c719323165587b2a10
Kino <10818333@my.uvu.edu>
2018-07-08 20:24:29 -0600

    Created projectile

---
+++ 1600Scripting/Assets/Scripts/Weapons/Ammo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour {
+
+	private Rigidbody projectile;
+	private void Start () {
+		projectile = GetComponent<Rigidbody>();
+		projectile.AddForce(0,0,100);
+	}
+}
