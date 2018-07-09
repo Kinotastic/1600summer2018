c55d8d36a66a577aba4ae20987de768c477cb562
Kino <10818333@my.uvu.edu>
2018-07-08 23:10:30 -0600

    Added ticker

---
+++ 1600Scripting/Assets/Scripts/WhileLoops.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoops : MonoBehaviour {
+
+	public bool StartGame = true;
+	public int i = 3;
+
+	void Start () {
+		StartCoroutine(Ticker());
+	}
+
+	IEnumerator Ticker()
+	{
+		while (i > 0)
+		{
+			print(i);
+			yield return new WaitForSeconds(1);
+			i--;
+		}
+
+		print("GO!");
+	}
+}
